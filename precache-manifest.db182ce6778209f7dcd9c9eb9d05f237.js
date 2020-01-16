3e30002d55b243f9814eb5a12b0bb0685fbada09
twohkz <web2bhw@gmail.com>
2020-01-16 19:12:10 +0100

    Updates

---
+++ precache-manifest.db182ce6778209f7dcd9c9eb9d05f237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924a7ef357f9af6360cdc9394fbf3d5a",
+    "url": "/mwat/index.html"
+  },
+  {
+    "revision": "a08200aad3f7d20d4b3e",
+    "url": "/mwat/static/css/main.4afc01ee.chunk.css"
+  },
+  {
+    "revision": "2f3e15e117e8ba377ac7",
+    "url": "/mwat/static/js/2.a32f096f.chunk.js"
+  },
+  {
+    "revision": "f53809ef2c6599bab1b665f3dfb08dc0",
+    "url": "/mwat/static/js/2.a32f096f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a08200aad3f7d20d4b3e",
+    "url": "/mwat/static/js/main.b1a11dd1.chunk.js"
+  },
+  {
+    "revision": "bc7a1870108a13917e67",
+    "url": "/mwat/static/js/runtime-main.36eac6b0.js"
+  }
+]);
